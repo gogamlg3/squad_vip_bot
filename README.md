064a8a8d2f00e681406ef516ff4e9fa21f8c7d03
gogamlg3 <sergeydubstep1611@gmail.com>
2024-02-09 20:55:24 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Бот для дискорда, с помощью которого можно узнать дату окончания вип на сервере сквада
+
+Работает вместе с: https://github.com/ar1ocker/Squad-Admin-Configurator
+
+Устанавливаем зависимости:
+```
+# pip install -r requirements.txt
+```
+Заполняем config.toml на примере файла config.example.toml
+
+Запускаем:
+```
+# python bot.py
+```
---
+++ bot.py
@@ -0,0 +1,88 @@
+import discord
+import asyncio
+import logging
+import requests
+import toml
+import json
+from discord import app_commands
+from datetime import datetime
+
+handler = logging.FileHandler(filename='discord_bot.log', encoding='utf-8', mode='w')
+
+CONFIG = toml.load("config.toml")
+webhook_url = CONFIG["WEBHOOK_URL"]
+admin_role = CONFIG["ROLE"]
+token = CONFIG["BOT_TOKEN"]
+rest_token = CONFIG["AUTH_TOKEN"]
+rest_url = CONFIG["REST_API_URL"]
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@tree.command(name="add", description="Добавить нового випа")
+@app_commands.describe(
+                       player_name = "Player name",
+                       steam_id = "SteamID",
+                       date = "На сколько дней выдать вип?")
+async def add_command(interaction: discord.Interaction, player_name: str, steam_id: str, date: str):
+    try:
+        if admin_role in [role.id for role in interaction.user.roles]:
+            data = {
+                   "steam_id": steam_id,
+                   "name": player_name,
+                   "comment": f"{interaction.user.name} webhook",
+                   "duration_until_end": date
+                   }
+
+            r = requests.post(webhook_url, data=data)
+            print(f"[{datetime.now()}]: {interaction.user.name} uses command /add")
+
+            if r.text != '{"detail":"Created"}':
+                await interaction.response.send_message(f"Ты что-то неправильно ввел\n\n{r.text}", ephemeral=True)
+
+            else:
+                await interaction.response.send_message(f"{interaction.user.mention} дал вип игроку '{player_name}' на срок {date} дня(-ей)")
+        else:
+            print(f"[{datetime.now()}]: {interaction.user.name} uses command /add without admin role")
+            await interaction.response.send_message("У вас нет прав на использование этой команды", ephemeral=True)
+
+
+    except Exception as e:
+        print(e)
+
+
+@tree.command(name="vip", description="Узнать время до истечения випки")
+@app_commands.describe(steam_id = "SteamID")
+async def vip_command(interaction: discord.Interaction, steam_id: str):
+    print(f"[{datetime.now()}]: {interaction.user.name} uses command /vip")
+    url_with_steam = f"{rest_url}?steam_id={steam_id}"
+    req = requests.get(url_with_steam, headers={"Authorization":f"Token {rest_token}"})
+    print(req.text)
+
+    if req.text in ["[]", '{"steam_id":["Введите число."]}']:
+        await interaction.response.send_message(f"У вас нет випа или вы ввели неверный SteamID", ephemeral=True)
+
+    else:
+        jlist = json.loads(req.text)[0]['date_of_end']
+
+        if jlist == None:
+            await interaction.response.send_message(f"Дата окончания вип: Бессрочно", ephemeral=True)
+
+        else:
+            date_list = jlist.split('-')
+            year = date_list[0]
+            month = date_list[1]
+            day = date_list[2].split('T')[0]
+            time = date_list[2].split('T')[1].split('+')[0].split('.')[0]
+            await interaction.response.send_message(f"Дата окончания вип: {day}.{month}.{year} {time} МСК", ephemeral=True)
+
+
+@client.event
+async def on_ready():
+    print(f"[{datetime.now()}]: {client.user} up!!")
+    synced = await tree.sync()
+    print(f'Synced {len(synced)} commands')
+
+client.run(token, log_handler=handler)
